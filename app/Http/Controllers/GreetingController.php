143c81802c6d5e0cd26ec779441f4c4daf2738a4
abdulazizkha08 <abdulaziz.khmail@gmail.com>
2024-05-07 17:12:48 +0500

    web.php

--- app/Http/Controllers/GreetingController.php
+++ app/Http/Controllers/GreetingController.php
@@ -2,23 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Greeting;
-
 class GreetingController extends Controller
 {
-    public function create(Request $request)
-    {
-        // Создаем новый экземпляр модели Greeting
-        $greeting = new Greeting();
-
-        // Устанавливаем значение атрибута body из запроса или из статического значения
-        $greeting->body = $request->input('body', 'Hello World');
-
-        // Сохраняем модель в базе данных
-        $greeting->save();
-
-        // Возвращаем сообщение об успешном создании
-        return 'Greeting created successfully!';
-    }
+   //
 }
--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Greeting;
-use Illuminate\Http\Request;
-
-use App\Http\Controllers\Controller;
 
 //Example 1.3.
 
-// Определение маршрута с использованием контроллера
-Route::get('/create-greeting', [GreetingController::class, 'create']);
-
-// Обработка маршрута в контроллере
-class GreetingController extends Controller
-{
-    public function create(Request $request)
-    {
-        // Создаем новый экземпляр модели Greeting
-        $greeting = new Greeting();
+Route::get('create-greeting', function () {
+        $greeting = new Greeting;
 
         // Устанавливаем значение атрибута body из запроса или из статического значения
-        $greeting->body = $request->input('body', 'Hello World');
+        $greeting->body = 'Hello, World!';
 
         // Сохраняем модель в базе данных
         $greeting->save();
 
         // Возвращаем сообщение об успешном создании
         return 'Greeting created successfully!';
-    }
-}
+});
 
 
 Route::get('first-greeting', function(){
